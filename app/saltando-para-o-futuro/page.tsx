d6ffd690f83df1b562651ede32acc47916f916b2
Adriano <adrianosenny@gmail.com>
2026-03-26 15:58:43 -0300

    feat: adicionar novas páginas e funcionalidade de redirecionamento

---
+++ app/saltando-para-o-futuro/page.tsx
@@ -0,0 +1,115 @@
+import Carousel from "@/components/Carousel";
+import Header from "@/components/Header";
+import { ArrowRight, Star, Trophy, Volleyball } from "lucide-react";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <Header />
+
+      <div className="container mx-auto pt-20 mb-4">
+        <h1 className="text-7xl flex flex-col gap-2 mb-10 font-medium">
+          <span>Domine a quadra.</span>
+          <span>
+            Voe com o{" "}
+            <span className="bg-gradient-to-r from-[#277f4a] to-[#ffd21f] bg-clip-text text-transparent">
+              Saltando para o Futuro.
+            </span>
+          </span>
+        </h1>
+
+        <a
+          href="#"
+          className="flex items-center gap-12 bg-zinc-950 w-fit text-white py-3.5 px-6 rounded-full"
+        >
+          Contato para Parcerias <ArrowRight size={18} />
+        </a>
+      </div>
+
+      <Carousel />
+
+      <div className="bg-zinc-950 w-full relative py-24 overflow-hidden">
+        <Image
+          src="/images/mao-bola.png"
+          alt="Logo do time Vôlei Glória"
+          width={700}
+          height={100}
+          className="absolute top-2/4 -translate-y-2/4 -left-24"
+        />
+
+        <div className="container mx-auto flex flex-col justify-end items-end">
+          <h2 className="text-white w-175 text-6xl leading-[1.2] mb-40">
+            Desde 2020, desenvolvendo talentos{" "}
+            <span className="opacity-50">dentro e fora da quadra.</span>
+          </h2>
+
+          <div className="flex w-175 gap-10">
+            <div className="flex flex-col flex-1">
+              <div className="border border-white/10 w-18 h-18 rounded-full flex items-center justify-center">
+                <svg width="26" height="26" viewBox="0 0 24 24">
+                  <defs>
+                    <linearGradient id="grad">
+                      <stop offset="0%" stopColor="#277f4a" />
+                      <stop offset="100%" stopColor="#ffd21f" />
+                    </linearGradient>
+                  </defs>
+
+                  <Volleyball stroke="url(#grad)" />
+                </svg>
+              </div>
+              <h3 className="text-white text-xl font-medium mt-4.5 mb-1.5">
+                Equipe Masculina
+              </h3>
+              <p className="text-white/50">
+                Performance, disciplina e evolução constante.
+              </p>
+            </div>
+
+            <div className="flex flex-col flex-1">
+              <div className="border border-white/10 w-18 h-18 rounded-full flex items-center justify-center">
+                <svg width="26" height="26" viewBox="0 0 24 24">
+                  <defs>
+                    <linearGradient id="grad">
+                      <stop offset="0%" stopColor="#277f4a" />
+                      <stop offset="100%" stopColor="#ffd21f" />
+                    </linearGradient>
+                  </defs>
+
+                  <Star stroke="url(#grad)" />
+                </svg>
+              </div>
+              <h3 className="text-white text-xl font-medium mt-4.5 mb-1.5">
+                Equipe Feminina
+              </h3>
+              <p className="text-white/50">
+                Talento, técnica e força coletiva.
+              </p>
+            </div>
+
+            <div className="flex flex-col flex-1">
+              <div className="border border-white/10 w-18 h-18 rounded-full flex items-center justify-center">
+                <svg width="26" height="26" viewBox="0 0 24 24">
+                  <defs>
+                    <linearGradient id="grad">
+                      <stop offset="0%" stopColor="#277f4a" />
+                      <stop offset="100%" stopColor="#ffd21f" />
+                    </linearGradient>
+                  </defs>
+
+                  <Trophy stroke="url(#grad)" />
+                </svg>
+              </div>
+              <h3 className="text-white text-xl font-medium mt-4.5 mb-1.5">
+                Adulto Masculino
+              </h3>
+              <p className="text-white/50">
+                Experiência aplicada ao alto rendimento.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,9 @@
+"use client";
+
+import { ChevronDown } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
+import { useEffect, useRef, useState } from "react";
 
 interface NavItem {
   label: string;
@@ -14,18 +18,88 @@ const navItems: NavItem[] = [
 ];
 
 export default function Header() {
+  const [open, setOpen] = useState(false);
+  const dropdownRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    function handleClickOutside(event: MouseEvent) {
+      if (
+        dropdownRef.current &&
+        !dropdownRef.current.contains(event.target as Node)
+      ) {
+        setOpen(false);
+      }
+    }
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => document.removeEventListener("mousedown", handleClickOutside);
+  }, []);
+
   return (
     <header className="w-full py-4">
       <div className="container mx-auto flex justify-between items-center">
-        <Link href="/" className="flex items-center gap-2">
-          <Image
-            src="/logo-verde.png"
-            alt="Logo do time Vôlei Glória"
-            width={50}
-            height={100}
-          />
-          <span className="text-2xl font-medium">Vôlei Glória</span>
-        </Link>
+        <div className="flex items-center gap-5 relative" ref={dropdownRef}>
+          <Link href="/" className="flex items-center gap-2">
+            <Image
+              src="/logo-verde.png"
+              alt="Logo do time Vôlei Glória"
+              width={50}
+              height={100}
+            />
+            <span className="text-2xl font-medium">Vôlei Glória</span>
+          </Link>
+
+          <button
+            onClick={() => setOpen((prev) => !prev)}
+            className="w-5 h-5 mt-px flex items-center cursor-pointer rounded-sm justify-center bg-slate-950/6"
+          >
+            <ChevronDown
+              size={16}
+              className={`transition-transform ${open ? "rotate-180" : ""}`}
+            />
+          </button>
+
+          {open && (
+            <div className="absolute top-14 w-full p-2.5 space-y-2 bg-white/80 rounded-sm border border-slate-950/15 flex flex-col">
+              <Link
+                href="/volei-gloria"
+                className="px-4 py-2 flex items-center font-semibold gap-3 bg-emerald-100 rounded-sm"
+              >
+                <div className="w-3.5 h-3.5 rounded-full border border-emerald-400 p-0.5">
+                  <div className="bg-emerald-400 w-full h-full rounded-full"></div>
+                </div>
+
+                <div className="flex items-center gap-1.5 text-slate-950">
+                  <Image
+                    src="/logo-verde.png"
+                    alt="Logo do time Vôlei Glória"
+                    width={35}
+                    height={100}
+                  />
+                  Vôlei Glória
+                </div>
+              </Link>
+              <Link
+                href="/saltando-para-o-futuro"
+                className="px-4 py-2 flex items-center font-semibold gap-3 bg-emerald-100 rounded-sm"
+              >
+                <div className="w-3.5 h-3.5 rounded-full border border-emerald-400 p-0.5">
+                  <div className="bg-emerald-400 w-full h-full rounded-full"></div>
+                </div>
+
+                <div className="flex items-center gap-1.5 text-slate-950">
+                  <Image
+                    src="/logo-verde.png"
+                    alt="Logo do time Vôlei Glória"
+                    width={35}
+                    height={100}
+                  />
+                  Projeto
+                </div>
+              </Link>
+            </div>
+          )}
+        </div>
 
         <nav className="flex items-center gap-10">
           {navItems.map((item) => (
